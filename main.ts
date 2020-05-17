9b74bebdbc5c695fd1cf4272895e80540cccb013
Bier-N <65338377+Bier-N@users.noreply.github.com>
2020-05-17 12:24:19 +0800

    Conditional blocks

--- main.ts
+++ main.ts
@@ -25,46 +25,47 @@ b d 1 1 1 1 1 1 d d d d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d d d 
 . . . . . . . . . . . b b b b b d d d 1 1 1 1 1 1 1 1 1 1 1 d d d b b b b b . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . b b b b b b b b b b b b b b b b b . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
-let mySprite2 = sprites.create(img`
-. . . . . . . . . . . c c c c c 6 6 6 6 6 . . . . . . . . . . . 
-. . . . . . . . c c c 4 4 4 4 4 4 4 4 4 4 6 6 6 . . . . . . . . 
-. . . . . . c c 4 4 4 4 4 4 4 4 4 b b 4 4 4 4 4 6 6 . . . . . . 
-. . . . . c b 4 4 4 4 b b 4 4 4 4 b 5 b 4 4 4 4 4 4 b . . . . . 
-. . . . e b 4 4 4 4 b 5 b 4 4 4 4 4 b 4 4 4 4 4 4 4 4 b . . . . 
-. . . e b b 4 4 4 4 4 b 4 4 4 4 4 4 4 4 4 4 b 4 4 4 4 4 6 . . . 
-. . e b 6 b b 4 4 4 4 4 4 4 4 4 b b 4 4 4 b 5 b 4 4 4 4 4 6 . . 
-. . e 6 b b 5 b 4 4 4 4 4 4 4 4 b 5 b 4 4 4 b 4 4 b b 4 4 e . . 
-. e 6 6 b 4 b 4 4 4 4 4 4 4 4 4 4 b 4 4 4 4 4 4 4 b 5 b 4 4 e . 
-. e 6 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b b 4 4 e . 
-e b 6 6 b 4 4 4 4 4 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b e 
-e b 6 6 b b 4 4 4 b 5 b 4 4 4 4 4 4 4 4 b b 4 4 4 4 4 4 4 4 b e 
-f b 6 6 6 b 4 4 4 b b 4 4 4 4 4 4 4 4 4 b 5 b 4 4 4 4 4 4 4 b f 
-f c b 6 6 6 b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b b 4 4 4 4 4 4 b c f 
-. f b b 6 6 6 6 b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b f . 
-. e f b b 6 6 6 6 6 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b f e . 
-. 8 6 f c b b 6 6 6 6 6 b b b 4 4 4 4 4 4 4 4 4 4 4 b c c 6 8 8 
-8 7 7 2 e f f c b b b b b b b b b b b b b b b b c f c 2 2 7 7 8 
-8 7 7 2 2 2 2 2 c c c c c c c c c c c c c c c c 2 2 2 2 6 6 7 8 
-f 8 6 6 6 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 7 6 6 8 6 f 
-f e f 8 6 6 6 7 7 7 6 6 6 6 7 7 7 7 7 7 6 6 6 7 7 7 7 f f f e f 
-f b f f 8 7 7 7 6 8 f 8 6 7 7 7 7 7 7 6 6 6 7 7 6 f f f f f b f 
-f b e f f e e f f f e f f 7 7 6 6 6 8 8 e f f e e e e f e b 6 f 
-f 6 b f f f e f f e e e e e e e e e e e e e f e e e e e b b 6 e 
-f 6 6 d d f f f f f e e e f f e f f e e e e e f f e e d b 4 6 e 
-. c 6 6 d d d 4 e f f f f f f e e e e e f f f f 4 d d b 4 6 e . 
-. f c 6 b 4 d d d d d d d d d d d d d d d d d d d b 4 4 4 e e . 
-. . f f 6 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 e e . . 
-. . . . f f b b b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 e e . . . . 
-. . . . . . f f e b b b b b b 4 4 4 4 4 4 4 4 e e e . . . . . . 
-. . . . . . . . . f f f f f f f c c c c c e e . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 0)
-mySprite.say("Catch as many as you can in one minute!")
+info.setScore(0)
+mySprite.setPosition(80, 120)
+mySprite.say("You have one minute! Catch as many as you can!")
+pause(6000)
 info.startCountdown(60)
-mySprite.setPosition(80, 110)
+let mySprite2 = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Food)
 mySprite2.setPosition(Math.randomRange(20, 140), 0)
 for (let index = 0; index < 120; index++) {
     mySprite2.y += 1
     pause(10)
+}
+if (mySprite.overlapsWith(mySprite2)) {
+    info.changeScoreBy(1)
+    mySprite2.setPosition(Math.randomRange(20, 140), 0)
+    mySprite2.vy += 2
+    for (let index = 0; index < 120; index++) {
+        mySprite2.y += 1
+        pause(10)
+    }
+} else {
+    mySprite2.setPosition(Math.randomRange(20, 140), 0)
+    for (let index = 0; index < 120; index++) {
+        mySprite2.y += 1
+        pause(10)
+    }
 }
